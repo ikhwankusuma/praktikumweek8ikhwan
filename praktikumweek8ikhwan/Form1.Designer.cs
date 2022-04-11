6073ebd47d1750758a783c0dd56dad6b5d130124
ikhwankusuma <ikhwan.kamara@gmail.com>
2022-04-11 09:37:12 +0700

    Add project files.

---
+++ praktikumweek8ikhwan/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace praktikumweek8ikhwan
+{
+    partial class praktikumweek8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbtimhome = new System.Windows.Forms.ComboBox();
+            this.cbtimaway = new System.Windows.Forms.ComboBox();
+            this.lbvs = new System.Windows.Forms.Label();
+            this.lbmanagerhome = new System.Windows.Forms.Label();
+            this.lbcaptainhome = new System.Windows.Forms.Label();
+            this.lbnamemanagerhome = new System.Windows.Forms.Label();
+            this.lbnamacapthome = new System.Windows.Forms.Label();
+            this.lbmanageraway = new System.Windows.Forms.Label();
+            this.lbnamamanageraway = new System.Windows.Forms.Label();
+            this.lbnamacapt = new System.Windows.Forms.Label();
+            this.lbnamacaptaway = new System.Windows.Forms.Label();
+            this.lbstadium = new System.Windows.Forms.Label();
+            this.lbnamastadium = new System.Windows.Forms.Label();
+            this.lbcapacity = new System.Windows.Forms.Label();
+            this.lbvalue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbtimhome
+            // 
+            this.cbtimhome.FormattingEnabled = true;
+            this.cbtimhome.Location = new System.Drawing.Point(33, 38);
+            this.cbtimhome.Name = "cbtimhome";
+            this.cbtimhome.Size = new System.Drawing.Size(144, 21);
+            this.cbtimhome.TabIndex = 0;
+            this.cbtimhome.SelectedIndexChanged += new System.EventHandler(this.cbtimhome_SelectedIndexChanged);
+            // 
+            // cbtimaway
+            // 
+            this.cbtimaway.FormattingEnabled = true;
+            this.cbtimaway.Location = new System.Drawing.Point(448, 38);
+            this.cbtimaway.Name = "cbtimaway";
+            this.cbtimaway.Size = new System.Drawing.Size(144, 21);
+            this.cbtimaway.TabIndex = 1;
+            this.cbtimaway.SelectedIndexChanged += new System.EventHandler(this.cbtimaway_SelectedIndexChanged);
+            // 
+            // lbvs
+            // 
+            this.lbvs.AutoSize = true;
+            this.lbvs.Location = new System.Drawing.Point(302, 46);
+            this.lbvs.Name = "lbvs";
+            this.lbvs.Size = new System.Drawing.Size(21, 13);
+            this.lbvs.TabIndex = 2;
+            this.lbvs.Text = "VS";
+            // 
+            // lbmanagerhome
+            // 
+            this.lbmanagerhome.AutoSize = true;
+            this.lbmanagerhome.Location = new System.Drawing.Point(30, 124);
+            this.lbmanagerhome.Name = "lbmanagerhome";
+            this.lbmanagerhome.Size = new System.Drawing.Size(55, 13);
+            this.lbmanagerhome.TabIndex = 3;
+            this.lbmanagerhome.Text = "Manager :";
+            // 
+            // lbcaptainhome
+            // 
+            this.lbcaptainhome.AutoSize = true;
+            this.lbcaptainhome.Location = new System.Drawing.Point(30, 157);
+            this.lbcaptainhome.Name = "lbcaptainhome";
+            this.lbcaptainhome.Size = new System.Drawing.Size(49, 13);
+            this.lbcaptainhome.TabIndex = 4;
+            this.lbcaptainhome.Text = "Captain :";
+            // 
+            // lbnamemanagerhome
+            // 
+            this.lbnamemanagerhome.AutoSize = true;
+            this.lbnamemanagerhome.Location = new System.Drawing.Point(149, 124);
+            this.lbnamemanagerhome.Name = "lbnamemanagerhome";
+            this.lbnamemanagerhome.Size = new System.Drawing.Size(49, 13);
+            this.lbnamemanagerhome.TabIndex = 5;
+            this.lbnamemanagerhome.Text = "..............";
+            // 
+            // lbnamacapthome
+            // 
+            this.lbnamacapthome.AutoSize = true;
+            this.lbnamacapthome.Location = new System.Drawing.Point(149, 157);
+            this.lbnamacapthome.Name = "lbnamacapthome";
+            this.lbnamacapthome.Size = new System.Drawing.Size(46, 13);
+            this.lbnamacapthome.TabIndex = 6;
+            this.lbnamacapthome.Text = ".............";
+            // 
+            // lbmanageraway
+            // 
+            this.lbmanageraway.AutoSize = true;
+            this.lbmanageraway.Location = new System.Drawing.Point(445, 124);
+            this.lbmanageraway.Name = "lbmanageraway";
+            this.lbmanageraway.Size = new System.Drawing.Size(55, 13);
+            this.lbmanageraway.TabIndex = 7;
+            this.lbmanageraway.Text = "Manager :";
+            // 
+            // lbnamamanageraway
+            // 
+            this.lbnamamanageraway.AutoSize = true;
+            this.lbnamamanageraway.Location = new System.Drawing.Point(557, 124);
+            this.lbnamamanageraway.Name = "lbnamamanageraway";
+            this.lbnamamanageraway.Size = new System.Drawing.Size(55, 13);
+            this.lbnamamanageraway.TabIndex = 8;
+            this.lbnamamanageraway.Text = "................";
+            // 
+            // lbnamacapt
+            // 
+            this.lbnamacapt.AutoSize = true;
+            this.lbnamacapt.Location = new System.Drawing.Point(445, 157);
+            this.lbnamacapt.Name = "lbnamacapt";
+            this.lbnamacapt.Size = new System.Drawing.Size(49, 13);
+            this.lbnamacapt.TabIndex = 9;
+            this.lbnamacapt.Text = "Captain :";
+            // 
+            // lbnamacaptaway
+            // 
+            this.lbnamacaptaway.AutoSize = true;
+            this.lbnamacaptaway.Location = new System.Drawing.Point(557, 157);
+            this.lbnamacaptaway.Name = "lbnamacaptaway";
+            this.lbnamacaptaway.Size = new System.Drawing.Size(46, 13);
+            this.lbnamacaptaway.TabIndex = 10;
+            this.lbnamacaptaway.Text = ".............";
+            // 
+            // lbstadium
+            // 
+            this.lbstadium.AutoSize = true;
+            this.lbstadium.Location = new System.Drawing.Point(239, 245);
+            this.lbstadium.Name = "lbstadium";
+            this.lbstadium.Size = new System.Drawing.Size(51, 13);
+            this.lbstadium.TabIndex = 11;
+            this.lbstadium.Text = "Stadium :";
+            // 
+            // lbnamastadium
+            // 
+            this.lbnamastadium.AutoSize = true;
+            this.lbnamastadium.Location = new System.Drawing.Point(361, 245);
+            this.lbnamastadium.Name = "lbnamastadium";
+            this.lbnamastadium.Size = new System.Drawing.Size(40, 13);
+            this.lbnamastadium.TabIndex = 12;
+            this.lbnamastadium.Text = "...........";
+            // 
+            // lbcapacity
+            // 
+            this.lbcapacity.AutoSize = true;
+            this.lbcapacity.Location = new System.Drawing.Point(239, 278);
+            this.lbcapacity.Name = "lbcapacity";
+            this.lbcapacity.Size = new System.Drawing.Size(54, 13);
+            this.lbcapacity.TabIndex = 13;
+            this.lbcapacity.Text = "Capacity :";
+            // 
+            // lbvalue
+            // 
+            this.lbvalue.AutoSize = true;
+            this.lbvalue.Location = new System.Drawing.Point(361, 278);
+            this.lbvalue.Name = "lbvalue";
+            this.lbvalue.Size = new System.Drawing.Size(31, 13);
+            this.lbvalue.TabIndex = 14;
+            this.lbvalue.Text = "........";
+            // 
+            // praktikumweek8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbvalue);
+            this.Controls.Add(this.lbcapacity);
+            this.Controls.Add(this.lbnamastadium);
+            this.Controls.Add(this.lbstadium);
+            this.Controls.Add(this.lbnamacaptaway);
+            this.Controls.Add(this.lbnamacapt);
+            this.Controls.Add(this.lbnamamanageraway);
+            this.Controls.Add(this.lbmanageraway);
+            this.Controls.Add(this.lbnamacapthome);
+            this.Controls.Add(this.lbnamemanagerhome);
+            this.Controls.Add(this.lbcaptainhome);
+            this.Controls.Add(this.lbmanagerhome);
+            this.Controls.Add(this.lbvs);
+            this.Controls.Add(this.cbtimaway);
+            this.Controls.Add(this.cbtimhome);
+            this.Name = "praktikumweek8";
+            this.Text = "Hasil Pertandingan";
+            this.Load += new System.EventHandler(this.praktikumweek8_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbtimhome;
+        private System.Windows.Forms.ComboBox cbtimaway;
+        private System.Windows.Forms.Label lbvs;
+        private System.Windows.Forms.Label lbmanagerhome;
+        private System.Windows.Forms.Label lbcaptainhome;
+        private System.Windows.Forms.Label lbnamemanagerhome;
+        private System.Windows.Forms.Label lbnamacapthome;
+        private System.Windows.Forms.Label lbmanageraway;
+        private System.Windows.Forms.Label lbnamamanageraway;
+        private System.Windows.Forms.Label lbnamacapt;
+        private System.Windows.Forms.Label lbnamacaptaway;
+        private System.Windows.Forms.Label lbstadium;
+        private System.Windows.Forms.Label lbnamastadium;
+        private System.Windows.Forms.Label lbcapacity;
+        private System.Windows.Forms.Label lbvalue;
+    }
+}
+
---
+++ praktikumweek8ikhwan/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace praktikumweek8ikhwan
+{
+    public partial class praktikumweek8 : Form
+    {
+        public praktikumweek8()
+        {
+            InitializeComponent();
+        }
+        public static string sqlconnection = ("server=localhost;uid=root;pwd=;database=premier_league");
+        public MySqlConnection sqlconnect = new MySqlConnection(sqlconnection);
+        public MySqlCommand sqlcomend;
+        public MySqlDataAdapter sqladapter;
+        public string sqlquery;
+        private void praktikumweek8_Load(object sender, EventArgs e)
+        {
+            try 
+            {
+                sqlconnect.Open();
+                DataTable team = new DataTable();
+                sqlquery = "Select team_name, team_id from team";
+                sqlcomend = new MySqlCommand(sqlquery, sqlconnect);
+                sqladapter = new MySqlDataAdapter(sqlcomend);
+                sqladapter.Fill(team);
+                cbtimhome.DataSource = team;
+                cbtimhome.DisplayMember = "team_name";
+                cbtimhome.ValueMember = "team_id";
+                cbtimaway.DataSource = team;
+                cbtimaway.DisplayMember = "team_name";
+                cbtimaway.ValueMember = "team_id";
+                
+            }
+            catch (Exception ex)
+            { 
+            MessageBox.Show(ex.Message);
+            }
+       
+
+        }
+
+        private void cbtimhome_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable team = new DataTable();
+                sqlquery = "SELECT manager_name as `manager` FROM team T,manager M WHERE T.manager_id = M.manager_id and T.team_id = '" + cbtimhome.SelectedValue.ToString() + "'"
+    ;
+                sqlcomend = new MySqlCommand(sqlquery, sqlconnect);
+                sqladapter = new MySqlDataAdapter(sqlcomend);
+                sqladapter.Fill(team);
+                lbnamemanagerhome.Text = team.Rows[0]["manager"].ToString();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void cbtimaway_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable team = new DataTable();
+            sqlquery = "SELECT manager_name as `manageraway` FROM team T,manager M WHERE T.manager_id = M.manager_id and T.team_id = '" + cbtimaway.SelectedValue.ToString() + "'"
+;
+            sqlcomend = new MySqlCommand(sqlquery, sqlconnect);
+            sqladapter = new MySqlDataAdapter(sqlcomend);
+            sqladapter.Fill(team);
+            lbnamamanageraway.Text = team.Rows[0]["manageraway"].ToString();
+        }
+    }
+}
